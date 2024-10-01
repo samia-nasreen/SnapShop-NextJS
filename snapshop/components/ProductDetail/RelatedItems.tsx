77609958f37c06d2903e8c8dbe7cef7672948b1a
Samia Nasreen <samia.nasreen@brainstation-23.com>
2024-10-01 18:33:28 +0600

    added server side fetching to related items in product details page

--- snapshop/components/ProductDetail/RelatedItems.tsx
+++ snapshop/components/ProductDetail/RelatedItems.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import ProductsGrid from '@/components/UI/ProductsGrid';
 import RedSubHeading from '@/components/UI/RedSubHeading';
 import GridSkeleton from '@/components/UI/GridSkeleton';
@@ -9,44 +6,65 @@ interface RelatedItemsProps {
   category: string;
 }
 
-const RelatedItems: React.FC<RelatedItemsProps> = ({ category }) => {
-  const [relatedProducts, setRelatedProducts] = useState([]);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+interface Rating {
+  rate: number;
+  count: number;
+}
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(
-          `/api/products/category/${category}?limit=4`
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch products');
-        }
-        const data = await response.json();
-        setRelatedProducts(data);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError('An unknown error occurred');
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rating;
+}
 
-    fetchProducts();
-  }, [category]);
+const fetchProducts = async (category: string) => {
+  try {
+    const response = await fetch(
+      `https://fakestoreapi.com/products/category/${category}`
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch products');
+    }
+
+    const data = await response.json();
+
+    const fetchedProducts = data.sort(() => 0.5 - Math.random()).slice(0, 4);
+
+    const products = fetchedProducts.map((product: Product) => ({
+      id: product.id,
+      name: product.title,
+      image: product.image,
+      discount: Math.floor(Math.random() * 50) + 10,
+      price: product.price,
+      originalPrice: (product.price * (1 + Math.random() * 0.5)).toFixed(2),
+      category: product.category,
+      rating: product.rating.rate,
+      ratingCount: product.rating.count,
+      description: product.description,
+    }));
+
+    return products;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+const RelatedItems: React.FC<RelatedItemsProps> = async ({ category }) => {
+  const relatedProducts = await fetchProducts(category);
 
   return (
     <div className="container mx-auto mt-16 mb-24 p-5">
       <div className="flex justify-between items-center mb-5">
         <RedSubHeading subHeading="Related Items" />
       </div>
-      {isLoading && <GridSkeleton />}
-      {!isLoading && <ProductsGrid products={relatedProducts || []} />}
-      {error && <p>Error occured</p>}
+      {!relatedProducts && <GridSkeleton />}
+      {relatedProducts && <ProductsGrid products={relatedProducts || []} />}
     </div>
   );
 };
