032654bcd84e74264fd1dba735b113305f81a64f
Samia Nasreen <samia.nasreen@brainstation-23.com>
2024-10-01 18:38:45 +0600

    added server side fetching to jsut for you section of wishlist page

--- snapshop/components/Wishlist/JustForYou.jsx
+++ snapshop/components/Wishlist/JustForYou.jsx
@@ -1,50 +1,53 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import RedSubHeading from '@/components/UI/RedSubHeading';
-import TransparentButton from '@/components/UI/TransparentButton';
+// import TransparentButton from '@/components/UI/TransparentButton';
 import ProductsGrid from '@/components/UI/ProductsGrid';
 import GridSkeleton from '@/components/UI/GridSkeleton';
+import LinkButton from '../UI/LinkButton';
 
-const JustForYou = () => {
-  const router = useRouter();
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch('/api/products?randomize=true&count=4');
-        if (!response.ok) {
-          throw new Error('Failed to fetch products');
-        }
-        const data = await response.json();
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    if (!response.ok) {
+      throw new Error('Failed to fetch products');
+    }
 
-    fetchProducts();
-  }, []);
+    const data = await response.json();
 
-  const handleSeeAllButton = () => {
-    router.push('/products');
-  };
+    const fetchedProducts = data.sort(() => 0.5 - Math.random()).slice(0, 4);
+
+    const products = fetchedProducts.map((product) => ({
+      id: product.id,
+      name: product.title,
+      image: product.image,
+      discount: Math.floor(Math.random() * 50) + 10,
+      price: product.price,
+      originalPrice: (product.price * (1 + Math.random() * 0.5)).toFixed(2),
+      category: product.category,
+      rating: product.rating.rate,
+      ratingCount: product.rating.count,
+      description: product.description,
+    }));
+
+    return products;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+const JustForYou = async () => {
+  const products = await fetchProducts();
 
   return (
     <div className="container mx-auto mt-16 mb-24 p-5">
       <div className="flex flex-row justify-between items-center mb-5">
         <RedSubHeading subHeading="Just For You" />
-        <TransparentButton text="See All" onClick={handleSeeAllButton} />
+        {/* <TransparentButton text="See All" onClick={handleSeeAllButton} /> */}
+        <LinkButton text="See All" href="/products" />
       </div>
-      {isLoading && <GridSkeleton />}
-      {!isLoading && <ProductsGrid products={products} />}
-      {error && <p>Error occured: {error}</p>}
+      {!products && <GridSkeleton />}
+      {products && <ProductsGrid products={products} />}
     </div>
   );
 };
