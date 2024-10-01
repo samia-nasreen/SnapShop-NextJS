4b0ebf8bd17215fb3e52570944a1c27e219b74da
Samia Nasreen <samia.nasreen@brainstation-23.com>
2024-10-01 18:21:15 +0600

    added server side fetching to top rated section of homepage

--- snapshop/components/Home/TopRatedSection/TopRatedSection.jsx
+++ snapshop/components/Home/TopRatedSection/TopRatedSection.jsx
@@ -1,54 +1,57 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import RedSubHeading from '@/components/UI/RedSubHeading';
-import Button from '@/components/UI/Button';
+import LinkButton from '@/components/UI/LinkButton';
 import Heading from '@/components/UI/Heading';
 import ProductsGrid from '@/components/UI/ProductsGrid';
 import GridSkeleton from '@/components/UI/GridSkeleton';
 
-const TopRatedSection = () => {
-  const router = useRouter();
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(
-          '/api/products?minRating=4&randomize=true&count=4'
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch products');
-        }
-        const data = await response.json();
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    if (!response.ok) {
+      throw new Error('Failed to fetch products');
+    }
 
-    fetchProducts();
-  }, []);
+    const data = await response.json();
 
-  const handleViewAllButton = () => {
-    router.push('/products');
-  };
+    const filteredProducts = data.filter((product) => product.rating.rate > 4);
+
+    const randomProducts = filteredProducts
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 4);
+
+    const products = randomProducts.map((product) => ({
+      id: product.id,
+      name: product.title,
+      image: product.image,
+      discount: Math.floor(Math.random() * 50) + 10,
+      price: product.price,
+      originalPrice: (product.price * (1 + Math.random() * 0.5)).toFixed(2),
+      category: product.category,
+      rating: product.rating.rate,
+      ratingCount: product.rating.count,
+      description: product.description,
+    }));
+
+    return products;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+const TopRatedSection = async () => {
+  const products = await fetchProducts();
 
   return (
     <div className="flash-sales mt-16 mb-24 px-4 bg-white relative">
       <RedSubHeading subHeading="This Month" />
       <div className="flex items-center justify-between mb-4 ">
         <Heading text="Top Rated Products" />
-        <Button text="View All" onClick={handleViewAllButton} />
+        <LinkButton text="View All" href="/products" />
       </div>
-      {isLoading && <GridSkeleton />}
-      {!isLoading && <ProductsGrid products={products} />}
-      {error && <p>Error occured: {error}</p>}
+      {!products && <GridSkeleton />}
+      {products && <ProductsGrid products={products} />}
     </div>
   );
 };
