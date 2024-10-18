62f8f0865b367f8d246dabde6ca03e8fcfcf3105
Samia Nasreen <samia.nasreen@brainstation-23.com>
2024-10-18 15:15:34 +0600

    made the language dropdown visible for smaller screens

--- snapshop/components/Layout/Header/Header.jsx
+++ snapshop/components/Layout/Header/Header.jsx
@@ -16,8 +16,8 @@ const Header = () => {
   };
 
   return (
-    <div className="hidden lg:flex bg-black text-white font-sans p-4 items-center justify-between h-12">
-      <div className="flex items-center flex-grow justify-center">
+    <div className="flex bg-black text-white font-sans p-4 items-center justify-between h-12">
+      <div className="hidden lg:flex items-center flex-grow justify-center">
         <span className="inline-block text-sm mr-2 ml-[130px]">
           {t('saleText')}
         </span>
@@ -25,11 +25,11 @@ const Header = () => {
           {t('shopNow')}
         </span>
       </div>
-      <div className="flex items-center relative">
+      <div className="flex items-center relative ml-auto">
         <select
           value={locale}
           onChange={handleLanguageChange}
-          className="custom-select text-sm mr-24"
+          className="custom-select text-sm mr-2 lg:mr-24"
         >
           <option value="en">English</option>
           <option value="fr">Français</option>
